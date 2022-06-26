af720293d16d107f1a587ea683bfef2278479820
LianSheng197 <ci.i.cxcvii@gmail.com>
2022-06-26 14:05:00 +0800

    微調手機介面、調整搜尋結果（直接搜全部）

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clan Messages</title>
-    <link rel="stylesheet" href="./css/main.css">
+    <link rel="stylesheet" href="./css/main.css?<?=$_SERVER['REQUEST_TIME']?>">
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Ubuntu">
 </head>
 
